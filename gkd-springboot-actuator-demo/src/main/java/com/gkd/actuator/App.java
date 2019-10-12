96198c02e1f60b06107c5bb00e97ecc689b32f87
nidagewcc <nidagewcc@163.com>
2019-10-12 17:29:26 +0800

    add spring boot actuator draft demo

---
+++ gkd-springboot-actuator-demo/src/main/java/com/gkd/actuator/App.java
@@ -0,0 +1,18 @@
+package com.gkd.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Weishuo Zhang
+ * @date 2019/10/12
+ * @description
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+        // TODO
+    }
+}
