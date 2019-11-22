40968c163dcc83df3e33e4659f3653f125a5a179
zhangweishuo <zhangweishuo@ecej.com>
2019-11-22 19:06:54 +0800

    x

---
+++ Autowired注解/gkd-autowired-dependency-demo/src/main/java/com/gkd/gkdautowireddependencydemo/GkdAutowiredDependencyDemoApplication.java
@@ -0,0 +1,18 @@
+package com.gkd.gkdautowireddependencydemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkdAutowiredDependencyDemoApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GkdAutowiredDependencyDemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ Autowired注解/gkd-autowired-dependency-demo/src/test/java/com/gkd/gkdautowireddependencydemo/GkdAutowiredDependencyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkd.gkdautowireddependencydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkdAutowiredDependencyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
